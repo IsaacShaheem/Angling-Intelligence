a07f558918cb604c1b2e2edce4cc5f5b9d686fb6
Isaac Shaheem <isaacshaheem@Isaacs-MacBook-Air-5.local>
2026-05-10 12:50:10 -0400

    Revamp frontend UX and lake detail experience

--- frontend/dist/assets/Navbar-5yrZcz61.js
+++ frontend/dist/assets/Navbar-5yrZcz61.js
@@ -1,4 +1,4 @@
-import{r as c,j as a}from"./index-DDLXMjGS.js";/**
+import{r as c,j as a}from"./index-77H4peEF.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
